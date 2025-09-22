8477fe54b590386d2b9c2468c2f132c470a94530
sachinjangili <sachinjangili@gmail.com>
2025-09-22 11:43:54 +0530

    functions

--- function.js
+++ function.js
@@ -1,7 +1,18 @@
-const add = () => {
-    var a = 58;
-    var b = 43;
+//functions - It is a block of code 
+
+function add(){
+    var a = 17;
+    var b = 18;
     document.write(a + b);
     console.log(a+b);
 }
 add();
+
+
+// const add = () => {
+//     var a = 58;
+//     var b = 43;
+//     document.write(a + b);
+//     console.log(a+b);
+// }
+// add();
