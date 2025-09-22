0e7e5bd9314d23adbc9468e0149e2b8c3315555d
sachinjangili <sachinjangili@gmail.com>
2025-09-22 12:38:32 +0530

    functions

--- function.js
+++ function.js
@@ -1,18 +1,39 @@
-//functions - It is a block of code 
+// // //functions - It is a block of code perform multiple task single time.
 
-function add(){
-    var a = 17;
-    var b = 18;
-    document.write(a + b);
-    console.log(a+b);
+// // function add(){
+// //     var a = 17;
+// //     var b = 18;
+// //     document.write(a + b);
+// //     console.log(a+b);
+// // }
+// // add();
+
+
+// // // const add = () => {
+// // //     var a = 58;
+// // //     var b = 43;
+// // //     document.write(a + b);
+// // //     console.log(a+b);
+// // // }
+// // // add();
+
+// //ES7
+
+// const add = (a,b) => console.log(a+b);
+// add(765,56);
+
+//hoisting - to move the declaration varialble to the top
+
+// console.log(b);
+// b = 43;                  //error: b is not defined, so we use hoisting
+
+let name = "Nani";
+
+function abc() {
+    let name = "Sachin";
+    document.write(`Hello, My name is ${name} `);
 }
-add();
+    document.write(`Hello, My name is ${name}`);
+abc(); 
 
 
-// const add = () => {
-//     var a = 58;
-//     var b = 43;
-//     document.write(a + b);
-//     console.log(a+b);
-// }
-// add();
