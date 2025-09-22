df09ab452b0fe76c6027bedf5a5c3b2a9830cb0c
sachinjangili <sachinjangili@gmail.com>
2025-09-22 10:30:44 +0530

    variables

---
+++ app.js
@@ -0,0 +1,15 @@
+//Types of variables
+//var, let, const
+//var - global variable
+//let,const - local variable 
+
+
+var a = 67;
+var a = 90;
+
+let b = 36;
+
+const c = 24;
+
+console.log(`value b is ${b}`);
+console.log(a);
